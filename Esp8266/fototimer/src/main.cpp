f00e0ac9c7a7638eff60f8585c7a3f6135b1aace
caiocgv <45643298+caiocgv@users.noreply.github.com>
2025-04-23 21:57:50 -0300

    added button to restart counter

--- Esp8266/fototimer/src/main.cpp
+++ Esp8266/fototimer/src/main.cpp
@@ -18,6 +18,10 @@ ESP8266WiFiClass Wifi;              // Create a Wifi object
 
 String text, newText, tempo, strTime, id, mode = "Inicio/Fim"; // Initialize variables                
 
+String reset_button = "<form action='/restart'> \
+                        <button type='submit' style='width: 150px;'>Reiniciar Contagem Atual</button> \
+                      </form>";
+
 int seconds, sensorValue, range = 100; 
 unsigned long sec_mill, previousMillis, interval = 100, start, finish; // Initialize variables for timing and sensor reading
 
@@ -28,6 +32,13 @@ void handle_root() {
     String root = file.readString();
     root.replace("{{text}}", text); // Replace the placeholder with the text from the file
     root.replace("{{mode}}", mode); // Replace the placeholder with the mode
+    
+    if (mode == "Inicio/Fim") {
+      root.replace("{{reset_button}}", "");
+    } else {
+      root.replace("{{reset_button}}", reset_button);
+    }
+
     file.close();    
     server.send(200, "text/html", root);
   } else {
@@ -201,6 +212,7 @@ void setup() {
   });
   server.on("/round_course", HTTP_GET, []() {
     mode = "Circuito Fechado";
+    tempo = "";
     handle_root();
   });
   server.on("/range_set", HTTP_POST, []() {
@@ -208,6 +220,11 @@ void setup() {
     range = server.arg("range").toInt();    
     }
     settings();
+  });
+  server.on("/restart", HTTP_GET, []() {
+    start = 0; // Restart the ESP8266
+    tempo = "";
+    handle_root();
   });
   server.begin(); // Start the server
   FileRead();
