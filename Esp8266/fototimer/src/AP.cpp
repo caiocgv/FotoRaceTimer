1e7a63e9a5dc9eabd45308f7a693cccdc685873b
caiocgv <45643298+caiocgv@users.noreply.github.com>
2025-07-05 16:25:15 -0300

    Update AP.cpp
    added scan function tests

--- Esp8266/fototimer/src/AP.cpp
+++ Esp8266/fototimer/src/AP.cpp
@@ -1,6 +1,19 @@
 #include <ESP8266WiFi.h>
 
 const char* ssid = "teste";
+String devices = ""; // String to hold the scanned devices
+
+
+void scan_all() { // Function to scan for all devices
+  Serial.println("Scanning for devices..."); // Print message to the serial monitor
+  int numDevices = WiFi.scanNetworks(false, true); // Scan for networks on all channels
+  WiFi.scanComplete(); // Wait for the scan to complete
+  if (numDevices > 0) { // If there are devices found
+    for (int i = 0; i < numDevices; i++) { // Loop through the devices
+      Serial.println(String(WiFi.SSID(i)) + " : " + String(WiFi.RSSI(i)) + " : " + String(WiFi.channel(i))); // Get the device information
+    }
+  }
+}
 
 void setup() {
     Serial.begin(115200);
@@ -11,7 +24,10 @@ void setup() {
 
     Serial.print("AP IP address: ");
     Serial.println(WiFi.softAPIP());
+    scan_all(); // Scan for all devices
+    
 }
+
 
 void loop() {
     // Your code here
